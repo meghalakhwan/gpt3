5eca725cce04e0978607ae3ae062612a1711093f
meghalakhwan <meghalakhwan9@gmail.com>
2024-06-05 19:43:53 +0530

    Add features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React from "react";
 
-
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Feature,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+import './App.css';
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className="App">
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Feature />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
