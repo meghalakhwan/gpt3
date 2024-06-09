4585bc7ce116a91044193baf4888a00ff7d3d301
meghalakhwan <meghalakhwan9@gmail.com>
2024-06-09 14:06:13 +0530

    Add assets

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import './navbar.css';
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='gpt3__navbar'>
+      <div className='gpt__navbar-links'>
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
     </div>
   )
 }
